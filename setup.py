8a8d860db73fb4290ad465843ed8db4e52241c8b
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2018-11-06 18:12:10 -0800

    changed file structure and added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Data Structures",
+    description="Python Data Structures.",
+    version=0.1,
+    author="Adrienne Karnoski",
+    author_email="adrienne.j.karnoski@gmail.com",
+    license='MIT',
+    py_modules=['linked_list'],
+    package_dir={'': 'src'}
+)
